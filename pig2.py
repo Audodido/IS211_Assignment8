d6d0d4b5029ef5f6a65af3b44f5c053dc09f3a49
connor <chanwick@gmail.com>
2021-10-20 16:20:04 -0400

    built factory

--- pig2.py
+++ pig2.py
@@ -1,2 +1,63 @@
 import argparse
 import random
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.overall_total = 0
+        self.turn_total = 0
+
+class Human(Player):
+        species = 'human'
+
+class ComputerPlayer(Player):
+        species = 'computer'
+
+# call this from Game class to instantiate players
+class PlayerFactory:
+    def getPlayer(self, species, name):
+        self.species = species
+        
+        if species == 'human':
+            return Human(name)
+        else:
+            return ComputerPlayer(name)
+    
+    
+    # # check to make sure player is human or computer
+    # for s in range(len(species)):
+    #     if species[s] in species_options:
+    #         #if it is, create an instance of proper type of player -- this is a Factory??
+    #         if species[s] == 'human':
+    #             players.append(Human(f'{s}'))
+    #         else:
+    #             players.append(ComputerPlayer(f'{s}'))
+
+
+class Die:
+    def __init__(self, sides=6):
+        self.sides = sides 
+
+    def roll(self):
+        dots = random.randint(1,6)
+        return dots
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--player1", help="'human' or 'computer'", type=str, required=False)
+    parser.add_argument("--player2", help="'human' or 'computer'", type=str, required=False)
+    args = parser.parse_args()
+    
+    species = [args.player1, args.player2]
+    players = []
+
+
+# just a test that the facotry is working 
+    fac = PlayerFactory()
+    player1 = fac.getPlayer(args.player1, '1')
+
+    print(f'player {player1.name} species: {player1.species}')
+
+  
--- sketch.py
+++ sketch.py
@@ -1,34 +1,81 @@
-class Person: 
-    def __init__(self): 
-        self.name = None 
-        self.gender = None 
+#################################################
+# example of factory pattern:
+
+# class Person: 
+#     def __init__(self): 
+#         self.name = None 
+#         self.gender = None 
   
-    def getName(self): 
-        return self.name 
+#     def getName(self): 
+#         return self.name 
 
-    def getGender(self): 
-        return self.gender 
+#     def getGender(self): 
+#         return self.gender 
 
 
-class Male(Person): 
-    def __init__(self, name): 
-        print("Hello Mr. " + name) 
+# class Male(Person): 
+#     def __init__(self, name): 
+#         print("Hello Mr. " + name) 
 
-class Female(Person): 
-    def __init__(self, name): 
-        print ("Hello Miss. " + name) 
+# class Female(Person): 
+#     def __init__(self, name): 
+#         print ("Hello Miss. " + name) 
 
-class Factory: 
+# class Factory: 
 
   
-    def getPerson(self, name, gender): 
-        if gender == 'M': 
-            return Male(name) 
-        if gender == 'F': 
-            return Female(name) 
+#     def getPerson(self, name, gender): 
+#         if gender == 'M': 
+#             return Male(name) 
+#         if gender == 'F': 
+#             return Female(name) 
 
-f = Factory()
+# f = Factory()
+
+# print(f.getPerson('connor', 'F'))
 
 
 
-print(f.getPerson('connor', 'F'))
+#################################################
+# example of proxy pattern - athlete (Real Subject) and coach (Proxy)
+from random import randint
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.majors = True
+        self.injured = False
+        self.pay = randint(1200000, 6000000)
+        self.injury = ''
+
+    def recovered(self):
+        self.injured = False
+
+class Coach:
+    def __init__(self, name):
+        self.name = name
+
+    def gotHurt(self, player, injury):
+        player.injured = True
+        self.player = player
+        self.injury = injury 
+
+    def sendtoAAA(self, player):
+        if player.injured == True:
+            player.majors = False
+            print(f'sending {player.name} down to AAA to recover from a {self.injury} injury')
+        else:
+            print(f'{player.name} is good and healthy. Let\'s keep him up here.')
+
+
+if __name__ == '__main__':
+    
+    rizz = Player('Anthony Rizzo')
+    boone = Coach('Aaron Boone')
+
+    print(rizz.majors)
+    boone.gotHurt(rizz, 'hamstring')
+    boone.sendtoAAA(rizz)
+    print(rizz.majors)
+
+    
