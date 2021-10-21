25f0f257e294c98ab8bc6699ef1735500d423c23
connor <chanwick@gmail.com>
2021-10-21 11:23:09 -0400

    Added to program

--- pig2.py
+++ pig2.py
@@ -1,37 +1,5 @@
 import argparse
-import random
-
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.overall_total = 0
-        self.turn_total = 0
-
-class Human(Player):
-        species = 'human'
-
-class ComputerPlayer(Player):
-        species = 'computer'
-
-# call this from Game class to instantiate players
-class PlayerFactory:
-    def getPlayer(self, species, name):
-        self.species = species
-        
-        if species == 'human':
-            return Human(name)
-        else:
-            return ComputerPlayer(name)
-    
-    
-    # # check to make sure player is human or computer
-    # for s in range(len(species)):
-    #     if species[s] in species_options:
-    #         #if it is, create an instance of proper type of player -- this is a Factory??
-    #         if species[s] == 'human':
-    #             players.append(Human(f'{s}'))
-    #         else:
-    #             players.append(ComputerPlayer(f'{s}'))
+from random import randint
 
 
 class Die:
@@ -39,8 +7,70 @@ class Die:
         self.sides = sides 
 
     def roll(self):
-        dots = random.randint(1,6)
+        dots = randint(1,6)
         return dots
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.overall_total = 0
+        self.turn_total = 0
+
+
+class Human(Player):
+        species = 'human'
+
+
+class ComputerPlayer(Player):
+        species = 'computer'
+
+
+# call this from Game class to instantiate players
+# https://www.youtube.com/watch?v=-a1PFtooGo4
+class PlayerFactory:
+    def get_player(self, species, name):
+        self.species = species
+        
+        if species == 'human':
+            return Human(name)
+        if species == 'computer':
+            return ComputerPlayer(name)
+        else:
+            print(f'invalid species type: {species}. Please choose \'human\' or \'computer\'')
+    
+
+class Game:
+    def __init__(self, die, species):
+        self.species = species #list of species types to be instantiated
+        self.die = die
+        self.players = []
+
+
+    def make_players(self): #creates instances of players from PlayerFactory
+        for p in range(len(self.species)):
+            player = PlayerFactory()
+            self.players.append(player.get_player(self.species[p], str(p)))
+
+
+    def is_game_over(self): # will have to eventually check for game duration as well
+        for player in self.players:
+            if player.overall_total >= 100:
+                return True
+            else:
+                return False
+
+    def turn(self, player):
+        pass
+
+    def message(self, player, msg):
+        self.player = player
+        #store messages for any occasion here:
+        msg_bank = {            
+            'turn_tot_msg' : f'Current turn-total for player {player.name}: {player.turn_total}',
+        }
+
+        return msg_bank[msg]
 
 
 if __name__ == '__main__':
@@ -48,16 +78,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--player1", help="'human' or 'computer'", type=str, required=False)
     parser.add_argument("--player2", help="'human' or 'computer'", type=str, required=False)
+    parser.add_argument("--timed", help="'y' or 'n'", type=str, required=False)
     args = parser.parse_args()
     
-    species = [args.player1, args.player2]
-    players = []
+    print("Welcome to Pig")
+    players = [args.player1, args.player2] 
+    die = Die()
 
+    game = Game(die, players)
+    game.make_players()
 
-# just a test that the facotry is working 
-    fac = PlayerFactory()
-    player1 = fac.getPlayer(args.player1, '1')
-
-    print(f'player {player1.name} species: {player1.species}')
-
-  
+    #below for testing
+    for p in game.players:
+        print(p.name, p.species)
+        print(game.message(p, 'turn_tot_msg'))
